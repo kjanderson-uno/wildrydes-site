1a8121a81c9f1eeb83038201ad35e04b1fffc4b8
kjanderson-uno <54546334+kjanderson-uno@users.noreply.github.com>
2020-11-02 15:44:16 -0600

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'eu-west-1_JTi25cDVB',
-        userPoolClientId: '5qi4adsulhj6c35qgngict3car',
+        userPoolId: 'us-east-1_6tBCgd2oQ',
+        userPoolClientId: '5q43725sk670cv8oco45r5v9ss',
         region: 'eu-west-1'
     },
     api: {
